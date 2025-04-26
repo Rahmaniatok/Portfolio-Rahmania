18f3fe8a518741be2911deba7a969d6a3b7eced9
Rahmaniatok <seseorangnia@gmail.com>
2025-04-26 15:42:00 +0700

    Update Navbarproject.tsx

--- app/Navbarproject.tsx
+++ app/Navbarproject.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Navbarproject (){
     return(
-    <div className="flex justify-between fixed top-0 w-screen z-10">
-            <Link className="p-8 hover:text-[#F1D302]" href={"/"}>
+    <nav className="fixed top-0 w-full bg-[#F1D302] z-10">
+        <div className="flex justify-between fixed top-0 w-screen z-10">
+            <Link className="p-8 hover:text-[#FFFFFF]" href={"/"}>
                 Back
             </Link>
-    </div>
+        </div>
+    </nav>
     )
 }
